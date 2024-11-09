e9e515700a31d4f55bde90486d44d07a79bac96e
D.J. Figueroa <djfigs10@gmail.com>
2024-11-09 08:19:34 -0800

    Update git ignore and add basic globe animations

---
+++ Assets/Scripts/EarthController.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class EarthController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    
+    private void Update()
+    {
+        
+    }
+}
